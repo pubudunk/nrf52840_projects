7bc85dfd9cd4a66a9823bfd7bf63f710504dc113
Pubudu Karunaratna <pubudu1@gmail.com>
2024-08-23 20:11:48 -0500

    feat: Add pca10059 ble soft device template project (empty implementation)

---
+++ 50-ble_app_template/main.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+    // Enter main loop.
+    for (;;)
+    {
+
+    }
+}
+
+
+/**
+ * @}
+ */
